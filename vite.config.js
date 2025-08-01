8fddddf23727109dbdb2d9dd90f30f2d40d2fa70
sajjadahammed <sajjad08@protonmail.com>
2025-08-01 23:13:33 +0530

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import path from "path"
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
